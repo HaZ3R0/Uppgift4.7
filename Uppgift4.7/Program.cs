8f1469e9d0c07dc234c394bee5e9afae618d1d45
HaZ3R0 <megamackan@outlook.com>
2023-10-24 18:29:40 +0200

    Add project files.

---
+++ Uppgift4.7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift_4._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett meddelnade?");
+            string meddelande = Console.ReadLine();
+            Console.WriteLine("Hur många steg åt höger ska meddelandet vara?");
+            string stegtext = Console.ReadLine();
+            int steg = int.Parse(stegtext);
+            
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                for (int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(meddelande[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
